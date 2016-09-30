4768e491b6a701bc7c4371e5a715ffceb681d0c2
pwang13 <pwang13@ncsu.edu>
2016-09-29 21:08:59 -0400

    try list comprehension

--- iterator/isinstance.py
+++ iterator/isinstance.py
@@ -1,6 +1,6 @@
 def items(x, depth=-1):
     if isinstance(x, (tuple, list)):
-        print(x)
+        # print(x)
         for y in x:
             for z in items(y, depth + 1):
                 yield z
@@ -8,10 +8,15 @@ def items(x, depth=-1):
         yield x
 
 
-out = []
-for x in items([10, [20, 30],
+# out = []
+# for x in items([10, [20, 30],
+#                 40,
+#                 [(50, 60, 70),
+#                  [80, 90, 100], 110]]):
+#     out += [x]
+
+out = [x for x in items ([10, [20, 30],
                 40,
                 [(50, 60, 70),
-                 [80, 90, 100], 110]]):
-    out += [x]
+                 [80, 90, 100], 110]]) if x > 20]
 print out
