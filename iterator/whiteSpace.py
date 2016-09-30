9b431cdbc46b2a915453c229acbdcf9787e7849c
pwang13 <pwang13@ncsu.edu>
2016-09-29 21:37:01 -0400

    try line comprehension

--- iterator/whiteSpace.py
+++ iterator/whiteSpace.py
@@ -1,7 +1,25 @@
 def remove_whitespaces(a_string):
-
     import string
     str = [x for x in a_string if x not in string.whitespace]
-    return ".".join(str)
+    return "".join(str)
 
-print remove_whitespaces("haha ha hhh hahah")
+
+def lines(string):
+    tmp = ''
+    for ch in string:
+        if ch == "\n":
+            yield tmp
+            tmp = ''
+        else:
+            tmp += ch
+    if tmp:
+        yield tmp
+
+def atleast_20(string):
+    return [remove_whitespaces(line) for line in lines(string) if remove_whitespaces(line) and len(line) > 20]
+
+print atleast_20("h hh \n"
+                         "\n"
+                         "   \n"
+                         "f jd jdjdk k djdfkaljfdkjfsljf"
+                         "")
