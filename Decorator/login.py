db2583b5b2c35461fc1429226369eeb92dd972fb
pwang13 <pwang13@ncsu.edu>
2016-10-04 21:20:59 -0400

    decorator with argument

--- Decorator/login.py
+++ Decorator/login.py
@@ -2,13 +2,16 @@ def printdebug_level(level = 0):  #add wrapper to recevie decorator's parameter
     def printdebug(func):
         def __decorator(user):
             print('enter the login, and debug level is: ' + str(level)) #print debug level
-            func(user)
+            res = func(user)
             print('exit the login')
+            return res
         return __decorator
     return printdebug    #return original decorator
 
 @printdebug_level()   #decorator's parameter, debug level set to 5
 def login(user):
-    print('in login:' + user)
+    print 'in login:' + user
+    msg = "success"
+    return msg
 
-login('jatsz')
+print login('jatsz')
